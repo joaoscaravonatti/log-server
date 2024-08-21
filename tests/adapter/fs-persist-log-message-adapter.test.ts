1a6252e5eda4df200704fd25f67062849855c212
Joao Scaravonatti <jvscaravonatti@gmail.com>
2024-08-21 11:10:15 -0300

    mock node:fs

--- tests/adapter/fs-persist-log-message-adapter.test.ts
+++ tests/adapter/fs-persist-log-message-adapter.test.ts
@@ -1,27 +1,28 @@
 import { FsPersisLogMessageAdapter } from '@/adapter/fs-persist-log-message-adapter'
+import { LogMessage } from '@/model/log-message'
 import path from 'node:path'
 import fs from 'node:fs'
-import { LogMessage } from '@/model/log-message'
 
-const TEST_FILE_PATH = [__dirname, '..', 'testfile.txt']
+jest.mock('node:fs', () => ({
+  promises: {
+    appendFile: jest.fn()
+  }
+}))
 
 describe('FsPersisLogMessageAdapter', () => {
   let sut: FsPersisLogMessageAdapter
 
   beforeAll(() => {
-    sut = new FsPersisLogMessageAdapter(TEST_FILE_PATH)
+    sut = new FsPersisLogMessageAdapter(['path'])
   })
 
-  afterAll(async () => {
-    await fs.promises.unlink(path.resolve(...TEST_FILE_PATH))
-  })
-
-  it('should write data in the provided file', async () => {
+  it('should call appendFile() correctly', async () => {
     const logMessage = LogMessage.create('id', 'info', 'content', new Date())
     await sut.persist(logMessage)
-    const buffer = await fs.promises.readFile(path.resolve(...TEST_FILE_PATH))
-    const file = buffer.toString()
 
-    expect(file).toEqual(`id,info,content,${logMessage.createdDate.toISOString()}\n`)
+    expect(fs.promises.appendFile).toHaveBeenCalledWith(
+      path.resolve('path'),
+      `id,info,content,${logMessage.createdDate.toISOString()}\n`
+    )
   })
 })
