68729d9dd4ea45f0884e96ef554b23aee677f67f
Joao Scaravonatti <jvscaravonatti@gmail.com>
2024-08-21 11:01:11 -0300

    implement graceful shutdown

--- src/main/index.ts
+++ src/main/index.ts
@@ -3,5 +3,12 @@ import { Server } from './server'
 (async () => {
   const server = new Server()
   await server.listen()
-  process.on('SIGTERM', () => server.close())
+  const STOP_EVENTS = ['SIGTERM', 'SIGINT'] as const
+  
+  for (const event of STOP_EVENTS) {
+    process.on(event, async () => {
+      await server.close()
+      process.exit(0)
+    })
+  }
 })()
