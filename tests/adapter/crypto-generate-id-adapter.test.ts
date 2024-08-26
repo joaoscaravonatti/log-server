ce48d201017a578ef967cce60fb94a25bf53da45
Joao Scaravonatti <jvscaravonatti@gmail.com>
2024-08-26 10:46:08 -0300

    mock node:crypto

--- tests/adapter/crypto-generate-id-adapter.test.ts
+++ tests/adapter/crypto-generate-id-adapter.test.ts
@@ -1,10 +1,22 @@
 import { CryptoGenerateIdAdapter } from '@/adapter/crypto-generate-id-adapter'
+import crypto from 'node:crypto'
+
+jest.mock('node:crypto', () => ({
+  randomUUID: jest.fn().mockReturnValue('id')
+}))
 
 describe('CryptoGenerateIdAdapter', () => {
+  it('should call randomUUID()', () => {
+    const sut = new CryptoGenerateIdAdapter()
+    sut.generate()
+
+    expect(crypto.randomUUID).toHaveBeenCalled()
+  })
+
   it('should return a string', () => {
     const sut = new CryptoGenerateIdAdapter()
     const result = sut.generate()
 
-    expect(typeof result).toBe('string')
+    expect(result).toBe('id')
   })
 })
